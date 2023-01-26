cc0d7cf10c2cfcbd9b917d97b62cc4791f4e66ae
Mubleem <102492190+Mublin@users.noreply.github.com>
2023-01-26 10:37:11 +0100

    added hamburger menu

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const sections = document.getElementsByTagName("section")
 const controls = document.getElementsByClassName("control")
 const home = document.getElementById("home")
 const nav = document.querySelector(".nav")
+const menu = document.querySelector(".fas")
+const navItems = document.querySelector(".nav-items")
+// console.log(menu)
 window.addEventListener("scroll", function navBar() {
     if (home.getBoundingClientRect().bottom <= 50) {
         nav.classList.add("active-nav")
     } else {
         nav.classList.remove("active-nav")
     }
+})
+menu.addEventListener("click", ()=>{
+    navItems.classList.toggle("active")
 })
 //50
